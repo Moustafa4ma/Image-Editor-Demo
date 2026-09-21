dc6d4aaefaefbb1e86269e7cb83cb10f3de562b7
Moustafa4ma <90367764+Moustafa4ma@users.noreply.github.com>
2026-09-21 10:50:41 +0300

    Update Image.h

--- Image.h
+++ Image.h
@@ -3,7 +3,7 @@
 #include<iostream>
 
 class Image{
-    enum ImageType{Grayscale = 2 , RGB = 3};
+    enum ImageType{Grayscale = 1 , RGB = 3};
     int width;
     int heigth;
     unsigned char *pixels;
